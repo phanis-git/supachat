18116434013efe746cf3b29937f697ed8497a8ee
phanis-git <phanipspkmec@gmail.com>
2026-04-11 17:01:26 +0530

    changes done

--- backend/app.py
+++ backend/app.py
@@ -42,6 +42,7 @@ def get_connection():
         port=5432,                                      # ✅ pooler port
         sslmode="require",
     )
+    
     print("✅ Connected successfully!")
     return conn
 
